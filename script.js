ae27dee4c5b8fe11129db9da4d3bb1b01650ae05
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-02-20 22:20:26 +0200

    revin la prima varianta

--- script.js
+++ script.js
@@ -4,19 +4,14 @@ fetch("https://api.peviitor.ro/v1/companies/?count=false")
   .then((response) => response.json())
   .then((data) => {
     data.companies.forEach((collaborator) => {
-      const collaboratorNameCapitalized =
-        collaborator.name.charAt(0).toUpperCase() +
-        collaborator.name.slice(1).toLowerCase();
-      collaborator.name = collaboratorNameCapitalized;
-
       const div = document.createElement("div");
       const title = document.createElement("h2");
       const image = document.createElement("img");
       const link = document.createElement("a");
 
-      image.src = `./Assets/${collaboratorNameCapitalized}.avif`;
-      title.textContent = collaboratorNameCapitalized;
-      link.href = `https://peviitor.ro/rezultate?q=${collaboratorNameCapitalized}&country=Rom%C3%A2nia&page=1`;
+      image.src = `./Assets/${collaborator.name}.avif`;
+      title.textContent = collaborator.name;
+      link.href = `https://peviitor.ro/rezultate?q=${collaborator.name}&country=Rom%C3%A2nia&page=1             `;
 
       link.appendChild(image);
       link.appendChild(title);
