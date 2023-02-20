1c4df0ccac29d3f392b435a696666d4e6bb77f7c
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-02-20 21:19:37 +0200

    am terminat de implementat dinamic datele din api, respectiv titlul, imaginea si a href-ul

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const CardContainer = document.querySelector(".card-container");
+
+fetch("https://api.peviitor.ro/v1/companies/?count=false")
+  .then((response) => response.json())
+  .then((data) => {
+    data.companies.forEach((collaborator) => {
+      const div = document.createElement("div");
+      const title = document.createElement("h2");
+      const image = document.createElement("img");
+      const link = document.createElement("a");
+
+      image.src = `./Assets/${collaborator.name}.avif`;
+      title.textContent = collaborator.name;
+      link.href = `https://peviitor.ro/rezultate?q=${collaborator.name}&country=Rom%C3%A2nia&page=1             `;
+
+      link.appendChild(image);
+      link.appendChild(title);
+      div.appendChild(link);
+      CardContainer.appendChild(div);
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+/*
 const collaborators = [
   {
     name: "Amazon",
@@ -34,3 +60,5 @@ collaborators.forEach((collaborator) => {
   div.appendChild(link);
   CardContainer.appendChild(div);
 });
+
+*/
