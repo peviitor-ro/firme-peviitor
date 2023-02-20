9fcee70bd16007e17edd455fd081dadf91270ae4
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-02-20 21:37:20 +0200

    proba

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ fetch("https://api.peviitor.ro/v1/companies/?count=false")
       const image = document.createElement("img");
       const link = document.createElement("a");
 
-      image.src = `./Assets/${collaborator.name}.avif`;
-      title.textContent = collaborator.name;
-      link.href = `https://peviitor.ro/rezultate?q=${collaborator.name}&country=Rom%C3%A2nia&page=1             `;
+      const collaboratorNameCapitalized =
+        collaborator.name.charAt(0).toUpperCase() +
+        collaborator.name.slice(1).toLowerCase();
+
+      image.src = `./Assets/${collaboratorNameCapitalized}.avif`;
+      title.textContent = collaboratorNameCapitalized;
+      link.href = `https://peviitor.ro/rezultate?q=${collaboratorNameCapitalized}&country=Rom%C3%A2nia&page=1`;
 
       link.appendChild(image);
       link.appendChild(title);
@@ -22,43 +26,3 @@ fetch("https://api.peviitor.ro/v1/companies/?count=false")
   .catch((error) => {
     console.error("Error:", error);
   });
-
-/*
-const collaborators = [
-  {
-    name: "Amazon",
-    imageSrc: "./Assets/Amazon.avif",
-  },
-  {
-    name: "Profi",
-    imageSrc: "./Assets/Profi.avif",
-  },
-  {
-    name: "Bosch",
-    imageSrc: "./Assets/Bosch.avif",
-  },
-  {
-    name: "Endava",
-    imageSrc: "./Assets/Endava.avif",
-  },
-];
-
-const CardContainer = document.querySelector(".card-container");
-
-collaborators.forEach((collaborator) => {
-  const div = document.createElement("div");
-  const title = document.createElement("h2");
-  const image = document.createElement("img");
-  const link = document.createElement("a");
-
-  image.src = collaborator.imageSrc;
-  title.textContent = collaborator.name;
-  link.href = `https://peviitor.ro/rezultate?q=${collaborator.name}&country=Rom%C3%A2nia&page=1             `;
-
-  link.appendChild(image);
-  link.appendChild(title);
-  div.appendChild(link);
-  CardContainer.appendChild(div);
-});
-
-*/
