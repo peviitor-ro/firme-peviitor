b07422638a800950999da3b90299bf086d8f94dd
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-28 14:30:52 +0300

    get images from api

--- javascript/script.js
+++ javascript/script.js
@@ -4,27 +4,36 @@ const cardContainer = document.querySelector(".card-container");
 const selectColaboratori = document.querySelector(".count-colaboratori");
 const searchInput = document.querySelector("#searchBar");
 let colaboratori = [];
+let logos = [];
 
 searchInput.addEventListener("input", (e) => {
   const dateInput = e.target.value.toLowerCase().replace(/\s+/g, "");
   const colaboratoriFiltrati = colaboratori.filter((colaborator) =>
     colaborator.name.toLowerCase().replace(/\s+/g, "").includes(dateInput)
   );
-  displayColaboratori(colaboratoriFiltrati);
+  displayColaboratori(colaboratoriFiltrati, logos);
 });
 
 fetch("https://api.peviitor.ro/v1/companies/?count=false")
   .then((response) => response.json())
   .then((data) => {
+    console.log(data);
     selectColaboratori.textContent = `avem scrapere pentru ${data.companies.length} de companii !`;
     colaboratori = data.companies;
-    displayColaboratori(colaboratori);
+  })
+  .then(() => {
+    fetch("https://api.peviitor.ro/v1/logo/")
+      .then((response) => response.json())
+      .then((d) => {
+        logos = d.companies;
+        displayColaboratori(colaboratori, logos);
+      });
   })
   .catch((error) => {
     console.log("Error:", error);
   });
 
-function displayColaboratori(colaboratori) {
+function displayColaboratori(colaboratori, logos) {
   cardContainer.innerHTML = "";
   colaboratori.forEach((collaborator) => {
     const div = document.createElement("div");
@@ -34,12 +43,22 @@ function displayColaboratori(colaboratori) {
 
     const allToLowerCase = collaborator.name.toLowerCase().replace(/\s+/g, "");
 
-    image.src = `./assets/${allToLowerCase}.png`;
-    image.alt = collaborator.name;
-
-    image.onerror = () => {
-      image.src = "./assets/logonotfound.png";
-    };
+    for (let i = 0; i < logos.length; i++) {
+      if (
+        allToLowerCase === logos[i].name.toLowerCase().replace(/\s+/g, "") &&
+        logos[i].logo !== null
+      ) {
+        image.src = logos[i].logo;
+        image.alt = collaborator.name;
+        break;
+      } else {
+        image.src = `./assets/${allToLowerCase}.png`;
+        image.alt = collaborator.name;
+        image.onerror = () => {
+          image.src = "./assets/logonotfound.png";
+        };
+      }
+    }
     title.textContent = allToLowerCase;
     link.href = `https://peviitor.ro/rezultate?q=${allToLowerCase}&country=Rom%C3%A2nia&page=1             `;
 
